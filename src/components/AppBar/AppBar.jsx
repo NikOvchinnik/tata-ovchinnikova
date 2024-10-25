08de489886952adfc3dfbcb4cc188d5c135387fb
NikOvchinnik <n.ovchinnik.s@gmail.com>
2024-10-25 16:46:51 +0300

    header

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import style from './AppBar.module.css';
+import Icon from '../Icon/Icon';
 
 const AppBar = () => {
   return (
@@ -15,20 +16,61 @@ const AppBar = () => {
           />
         </a>
         <nav className={style.navContainer}>
-          <NavLink to="/">Каталог</NavLink>
-          <NavLink to="/masterclass">Майстер-класи</NavLink>
-          <NavLink to="/sale">Sale</NavLink>
-          <NavLink to="/about">Про нас</NavLink>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? style.navItemActive : style.navItem
+            }
+          >
+            Каталог
+          </NavLink>
+          <NavLink
+            to="/masterclass"
+            className={({ isActive }) =>
+              isActive ? style.navItemActive : style.navItem
+            }
+          >
+            Майстер-класи
+          </NavLink>
+          <NavLink
+            to="/sale"
+            className={({ isActive }) =>
+              isActive
+                ? `${style.navItemActive} ${style.navItemSale}`
+                : `${style.navItem} ${style.navItemSale}`
+            }
+          >
+            Sale
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive ? style.navItemActive : style.navItem
+            }
+          >
+            Про нас
+          </NavLink>
         </nav>
         <address className={style.addressContainer}>
           <ul className={style.addressList}>
-            <li>
-              <a href="https://www.instagram.com/_tata_ovchinnikova/">
+            <li className={style.addressItem}>
+              <a
+                href="https://www.instagram.com/_tata_ovchinnikova/"
+                className={style.adressLink}
+              >
+                <Icon
+                  id="insta"
+                  width="15"
+                  height="15"
+                  className={style.instaIcon}
+                />
                 _tata_ovchinnikova
               </a>
             </li>
-            <li>
-              <a href="tel:+380502803612">+380502803612</a>
+            <li className={style.addressItem}>
+              <a href="tel:+380502803612" className={style.adressLink}>
+                +380502803612
+              </a>
             </li>
           </ul>
         </address>
---
+++ src/components/Icon/Icon.jsx
@@ -0,0 +1,15 @@
+const Icon = ({ id, width, height, className = '' }) => {
+  return (
+    <svg
+      className={`${className}`}
+      style={{ background: 'transparent' }}
+      width={width}
+      height={height}
+      aria-hidden="true"
+    >
+      <use href={`/public/icon/icons.svg#icon-${id}`}></use>
+    </svg>
+  );
+};
+
+export default Icon;
