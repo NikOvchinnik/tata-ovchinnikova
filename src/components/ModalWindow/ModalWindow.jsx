42abb1043707e7c9e320ae0d62892ab8b0c449bd
NikOvchinnik <n.ovchinnik.s@gmail.com>
2025-01-07 19:13:44 +0200

    modal test

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -78,24 +78,7 @@ const ModalWindow = ({
             <div className={style.imgContainer}>
               <ImageGallery
                 items={galleryImages}
-                showThumbnails={false}
-                showFullscreenButton={true}
-                renderLeftNav={onClick => (
-                  <button
-                    className={`${style.navButton} ${style.navButtonLeft}`}
-                    onClick={onClick}
-                  >
-                    <Icon id="keyboard_arrow_left" className={style.iconNav} />
-                  </button>
-                )}
-                renderRightNav={onClick => (
-                  <button
-                    className={`${style.navButton} ${style.navButtonRight}`}
-                    onClick={onClick}
-                  >
-                    <Icon id="keyboard_arrow_right" className={style.iconNav} />
-                  </button>
-                )}
+
               />
               {card.isNew && (
                 <div className={style.newContainer}>
